c05a8267d36e48a628df25564ada09b9443aa4c7
Anukul k <anukulsingh125@gmail.com>
2020-08-14 11:47:50 +0530

    React redux form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const store = ConfigureStore();
 
 class App extends Component {
 
- 
+  
   render() {
   return (
+    <Provider store={store}>
     <BrowserRouter>
       <div>
       
@@ -16,6 +20,7 @@ class App extends Component {
      
       </div>
     </BrowserRouter>
+    </Provider>
   );
  }
 }
